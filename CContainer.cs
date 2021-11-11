d869f8f39b2720d81ddc05fc27eb34349b293356
Lenovo <Lenovo@LENOVO-PC19>
2021-11-11 21:03:20 +0200

    Добавьте файлы проекта.

---
+++ CContainer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+namespace LB_3
+{
+    class CContainer
+    {
+        private PictureBox pictBox;
+        private List<Shape> shapes;
+        public CContainer(PictureBox _pictBox)
+        {
+            pictBox = _pictBox;
+            shapes = new List<Shape>();
+        }
+        // Малювання всіх елементів контейнеру
+        public void Draw()
+        {
+            for(int index = 0; index < shapes.Count; index++)
+            {
+                shapes[index].Draw(pictBox);
+            }
+        }
+       // Отримання кількості елементів у контейнері
+        public int GetLength()
+        {
+            return shapes.Count;
+        }
+        public string ToString(int index)
+        {
+            string out1 = Convert.ToString(shapes[index].GetTyp()) + " " + Convert.ToString(shapes[index].GetX()) + " " + Convert.ToString(shapes[index].GetY()) + " "
+                + Convert.ToString(shapes[index].GetSide1()) + " " + Convert.ToString(shapes[index].GetSide2()) + " " + Convert.ToString(shapes[index].GetColor());
+            return out1;
+        }
+
+        // Додавання нового об'єкта у контейнер, на задану позицію.
+        public void AddAt(int type, float x, float y, float width, float heigth, int color, int index)
+        {
+            if (type == -1) { type = 0; }
+            Shape ob;
+            switch (type)
+            {
+                case 0:
+                    ob = new Rectangle(type, x, y, width, heigth, color);
+                    shapes.Insert(index, ob);
+                    break;
+                case 1:
+                    ob = new Circle(type, x, y, width, heigth, color);
+                    shapes.Insert(index, ob);
+                    break;
+                case 2:
+                    ob = new SixAngle(type, x, y, width, heigth, color);
+                    shapes.Insert(index, ob);
+                    break;
+            }
+        }
+        // Додавання нового об'єкта до контейнеру, на останнє місце.
+        public void Add(int type, float x, float y, float width, float heigth, int color)
+        {
+            AddAt(type, x, y, width, heigth, color, shapes.Count);
+        }
+        // Видалення об'єкту з контейнера
+        public void Remove(int index)
+        {
+            shapes.RemoveAt(index);
+        }
+
+    }
+}
---
+++ Шейп/Shape.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace LB_3
+{
+    abstract class Shape
+    {
+        protected int _type;
+        protected float _x;
+        protected float _y;
+        protected float _side1;
+        protected float _side2;
+        protected int _colorN;
+        protected Color _color;
+        protected Shape()
+        {
+
+        }
+        protected Shape(int type, float x, float y, float width, float heigth, int color)
+        {
+
+        }
+        public float GetX()
+        {
+            return _x;
+        }
+        public float GetY()
+        {
+            return _y;
+        }
+        public float GetSide1()
+        {
+            return _side1;
+        }
+        public float GetSide2()
+        {
+            return _side2;
+        }
+        public int GetColor()
+        {
+            return _colorN;
+        }
+        public int GetTyp()
+        {
+            return _type;
+        }
+
+        abstract public void Draw(PictureBox pictBox);
+
+    }
+}
